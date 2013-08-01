6c6987fc96aec8f9f71423de23d839b42946b74e
Darren Cauthon <darren@cauthon.com>
2013-07-31 23:49:08 -0500

    Rename to more appropriate name.

--- lib/red_alert.rb
+++ lib/red_alert.rb
@@ -1,13 +1,11 @@
-require "red_alert/check"
+require "red_alert/sensor"
 require "red_alert/version"
 
 module RedAlert
   def self.run
     require 'daemons'
     Daemons.run_proc('red_alert', { ARGV: ['run'] }) do
-      loop do
-        Check.fire!
-      end
+      loop { Sensor.scan! }
     end
   end
 end
---
+++ lib/red_alert/sensor.rb
@@ -0,0 +1,15 @@
+module RedAlert
+  class Sensor
+    def self.inherited base
+      @sensors ||= []
+      @sensors << base
+    end
+
+    def self.scan!
+      raise "no sensors defined" unless @sensors
+      @sensors.each do |sensor|
+        sensor.new.scan!
+      end
+    end
+  end
+end
